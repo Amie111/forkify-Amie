fe3ef305278c871f3e1b9509b9b61f2b6a4b7630
MIN YAN <minyanhhuc@gmail.com>
2024-11-13 16:53:05 +0900

    Display number of pages between pagination buttons

--- src/views/paginationView.js
+++ src/views/paginationView.js
@@ -25,32 +25,48 @@ class PaginationView extends View {
       this._data.results.length / this._data.resultsPerPage
     );
 
-    // 1）当前页是第一页，且页数大于1时：只显示下一页按钮
+    // Add current page display markup
+    const currentPageMarkup = `
+       <div class="pagination__current">Page ${curPage}</div>
+     `;
+
+    // 1）First page with more pages（当前页是第一页，且页数大于1时：只显示下一页按钮）
     if (curPage === 1 && numPage > 1) {
-      return `<button  data-goto = "${
+      return `<div class="pagination__container">
+          <div></div>
+          ${currentPageMarkup}
+      <button  data-goto = "${
         curPage + 1
       }" class="btn--inline pagination__btn--next">
             <span>Page ${curPage + 1}</span>
             <svg class="search__icon">
               <use href="${icons}#icon-arrow-right"></use>
             </svg>
-          </button>`;
+          </button>
+          </div>`;
     }
 
-    // 2）当前页是最后一页，且页数大于1时：只显示上一页按钮
+    // 2） Last page（当前页是最后一页，且页数大于1时：只显示上一页按钮）
     if (curPage === numPage && numPage > 1) {
-      return `<button data-goto = "${
+      return `
+      <div class="pagination__container">
+      <button data-goto = "${
         curPage - 1
       }" class="btn--inline  pagination__btn--prev">
             <svg class="search__icon">
               <use href="${icons}#icon-arrow-left"></use>
             </svg>
             <span>Page ${curPage - 1}</span>
-          </button>`;
+          </button>
+          ${currentPageMarkup}
+           <div></div>
+        </div>`;
     }
-    // 3）当前页是中间页时，同时显示上一页和下一页按钮
+    // 3）Middle page（当前页是中间页时，同时显示上一页和下一页按钮）
     if (curPage < numPage) {
-      return `<button data-goto = "${
+      return `
+      <div class="pagination__container">
+      <button data-goto = "${
         curPage - 1
       }" class="btn--inline pagination__btn--prev">
             <svg class="search__icon">
@@ -58,6 +74,7 @@ class PaginationView extends View {
             </svg>
             <span>Page ${curPage - 1}</span>
           </button>
+          ${currentPageMarkup}
          <button data-goto = "${
            curPage + 1
          }" class="btn--inline pagination__btn--next">
@@ -65,10 +82,16 @@ class PaginationView extends View {
             <svg class="search__icon">
               <use href="${icons}#icon-arrow-right"></use>
             </svg>
-          </button> `;
+          </button> 
+          </div>`;
     }
-    // 4）只有1页时，不显示
-    return '';
+    // 4）Only one page（只有1页时，只显示当前页码）
+    return `
+    <div class="pagination__container">
+    <div></div>
+    ${currentPageMarkup}
+    <div></div>
+  </div>`;
   }
 }
 
